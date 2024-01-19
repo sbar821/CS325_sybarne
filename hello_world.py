cf2bcc8b91929a3e9a4d3640f3122d4dbc121245
sbar821 <sydneybarnett821@gmail.com>
2024-01-18 22:54:52 -0600

    added hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("hello, world!\n")
+
+user_num=input("Give me a number, I'll do some math! I'll multiply it by two! :D\n")
+
+tmp = int(user_num)*3
+
+print(str(tmp) + ". Wait a minute. I think I did it wrong. :(\n")
+
+tmp = int(user_num)*2
+
+print("Let me try again. does " + str(tmp) + " sound right? :)\n")
